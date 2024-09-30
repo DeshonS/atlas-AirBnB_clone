1fe278d3f483933d7e39f2299871c65da15371c6
DeshonS <deshonjewell@gmail.com>
2024-09-30 01:30:54 -0500

    added storage to base_model in initialization and save

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """BaseModel Class"""
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel():
     def __init__(self, *args, **kwargs):
@@ -15,12 +16,14 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
         
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
         
     def to_dict(self):
         instance_dict = self.__dict__.copy()
