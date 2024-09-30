b44474743ec8e14fa60ede9be413a02786d001ba
DeshonS <deshonjewell@gmail.com>
2024-09-30 01:39:10 -0500

    restructured documentation

--- console.py
+++ console.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Entry point of the command interpreter
-"""
+"""Entry point of the command interpreter"""
 
 
 import cmd
@@ -12,9 +10,7 @@ import shlex
 
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Command processor
-    """
+    """Command processor"""
 
     prompt = "(hbnb)" #prompt text
     l_classes = ['BaseModel', 'User',]
@@ -22,9 +18,7 @@ class HBNBCommand(cmd.Cmd):
     l_c = ['count']
 
     def precmd(self, arg):
-        """
-        parses command input
-        """
+        """parses command input"""
         if '.' in arg and '(' in arg and ')' in arg:
             cls = arg.split('.')
             cnd = cls[1].split('(')
@@ -34,21 +28,15 @@ class HBNBCommand(cmd.Cmd):
         return arg
 
     def help_help(self):
-        """
-        Prints help command description
-        """
+        """Prints help command description"""
         print("Provides description of a given command")
 
     def emptyline(self):
-        """
-        do nothing when empty line
-        """
+        """do nothing when empty line"""
         pass
 
     def do_count(self, cls_name):
-        """
-        counts number of instances of a class
-        """
+        """counts number of instances of a class"""
         count = 0
         all_objs = storage.all()
         for k, v in all_objs.items():
@@ -58,15 +46,11 @@ class HBNBCommand(cmd.Cmd):
         print(count)
 
     def do_quit(self, line):
-        """
-        Quit command to exit the command interpreter
-        """
+        """Quit command to exit the command interpreter"""
         return True
 
     def do_EOF(self, line):
-        """
-        EOF command to exit the command interpreter
-        """
+        """EOF command to exit the command interpreter"""
         return True
 
 
