531abfd86281aaf2fd389f748522c635c947fcf2
DeshonS <deshonjewell@gmail.com>
2024-10-01 20:22:28 -0500

    added colon to end of if statement (line 49)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,7 +46,7 @@ class FileStorage:
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
-        if os.path.exists(self.__file_path)
+        if os.path.exists(self.__file_path):
             try:
                 with open(self.__file_path, 'r') as file:
                     obj_dict = json.load(file)
