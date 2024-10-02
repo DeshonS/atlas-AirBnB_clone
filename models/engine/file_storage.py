e5afdf7ab8299b1602bee9dc7a0b876600127c30
DeshonS <deshonjewell@gmail.com>
2024-10-01 20:09:40 -0500

    updated reload in FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,6 +12,13 @@ reload - deserializes JSON file to __objects if file exists, otherwise does noth
 """
 import json
 import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -24,43 +31,38 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         return self.__objects
-    
+
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
-        
+
     def save(self):
         with open(self.__file_path, 'w') as file:
             obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
             json.dump(obj_dict, file)
-            
+
     def reload(self):
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as file:
-                obj_dict = json.load(file)
-                from models.base_model import BaseModel
-                for key, val in obj_dict.items():
-                    cls = val['__class__']
-                    if cls == 'BaseModel':
-                        self.__objects[key] = BaseModel(**val)
-                    if cls == 'User':
-                        from models.user import User
-                        self.__objects[key] = User(**val)
-                    if cls == 'City':
-                        from models.city import City
-                        self.__objects[key] = City(**val)
-                    if cls == 'State':
-                        from models.state import State
-                        self.__objects[key] = State(**val)
-                    if cls == 'Amenity':
-                        from models.amenity import Amenity
-                        self.__objects[key] = Amenity(**val)
-                    if cls == 'Place':
-                        from models.place import Place
-                        self.__objects[key] = Place(**val)
-                    if cls == 'Review':
-                        from models.review import Review
-                        self.__objects[key] = Review(**val)
+        if os.path.exists(self.__file_path) and os.path.getsize(self.__file_path) > 0:
+            try:
+                with open(self.__file_path, 'r') as file:
+                    obj_dict = json.load(file)
+
+                    classes = {'BaseModel': BaseModel,
+                               'User': User,
+                               'State': State,
+                               'City': City,
+                               'Amenity': Amenity,
+                               'Place': Place,
+                               'Review': Review
+                               }
+
+                    for key, val in obj_dict.items():
+                        cls_name = val['__class__']
+                        cls = classes.get(cls_name)
+                        if cls:
+                            self.__objects[key] = cls(**val)
+            except Exception as e:
+                print(f"Error loading from {self.__file_path}: {e}")
